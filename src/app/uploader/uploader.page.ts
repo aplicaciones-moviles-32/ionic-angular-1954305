7870243417c311d52968d89a4d98fb73f8ae3e30
manuel.juarezml@uanl.edu.mx <manuel.juarezmelchor@gmail.com>
2022-05-27 20:58:58 -0500

    Actualizando Uploader para subir imagenes

--- src/app/uploader/uploader.page.ts
+++ src/app/uploader/uploader.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import uploadcare from 'uploadcare-widget/uploadcare.lang.en.min.js';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-uploader',
@@ -10,10 +11,25 @@ import uploadcare from 'uploadcare-widget/uploadcare.lang.en.min.js';
 export class UploaderPage implements OnInit {
 
   imageURL: string;
+  desc: string;
 
-  constructor(public http: HttpClient) { }
+  constructor(
+    public http: HttpClient,
+    public user: UserService) { }
 
   ngOnInit() {
+  }
+
+  createPost(){
+    const image = this.imageURL;
+    const desc = this.desc;
+
+   // this.afstore.toJSON('users/{this.user.getUID}').update({
+    //  posts: Firestore.FieldValue.arrayUnion({
+      //  image,
+     //   desc
+    //  })
+    // });
   }
 
   fileChanged(event) {
